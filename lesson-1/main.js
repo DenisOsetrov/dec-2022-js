c20cd08eefb9ebcadded13a021b64673eb2ab168
DenisOsetrov <dvosetrov@gmail.com>
2023-02-01 17:05:31 +0200

    lesson 1

---
+++ lesson-1/main.js
@@ -0,0 +1,39 @@
+let str = 'hello';
+console.log(str);
+let wor = 'owu';
+console.log(wor);
+let com = 'com';
+console.log(com);
+let ua = 'ua';
+console.log(ua);
+let num = 1;
+console.log(typeof num, num);
+let num2 = 10;
+console.log(num2);
+let num3 = -999;
+console.log(num3);
+let num4 = 123;
+console.log(num4)
+const PI = 3.14;
+console.log(PI)
+let num6 = 2.7;
+console.log(num6);
+let num7 = 16;
+console.log(num7)
+let num8 = true;
+console.log(num8);
+let num9 = false;
+console.log(num9);
+
+let firstName = 'Osetrov Denis Volodymyrovycv';
+let middleName = 'Osetrov Denis Volodymyrovycv';
+let lastName = 'Osetrov Denis Volodymyrovycv';
+let username = (firstName + ' ' +  middleName + ' ' + lastName)
+console.log(username);
+
+let a = 100;
+console.log(typeof a, a);
+let b = '100';
+console.log(typeof b, b)
+let c = true;
+console.log(typeof c,c)
